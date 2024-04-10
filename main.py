51703b5017da2fe95aac7a9540799f5b9a16a372
Jonathan <jonbalsicas14@gmail.com>
2024-04-10 12:33:58 +0800

    Finished Product

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+PLACEHOLDER = "[name]"
 
-#Read data from starting_letter and store it to a variable "invitation"
+with open("Input/Names/invited_names.txt") as guest:
+    guest_list = guest.readlines()
+    print(guest_list)
 
-with open("C:/Users/ADMIN/PycharmProjects/Mail Merge Project Start/Input/Letters/starting_letter.txt") as file:
-    contents = file.read()
-    print(contents)
-    
+with open("Input/Letters/starting_letter.txt", "r") as file:
+    letter = file.read()
+    for name in guest_list:
+        stripped_name = name.strip()
+        invitation = letter.replace(PLACEHOLDER,stripped_name)
+        with open(f"Output/ReadyToSend/letter_for_{stripped_name}.txt", mode= "w") as completed_invitation:
+            completed_invitation.write(invitation)
